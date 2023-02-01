dfb8f87afa41dd2ca4ef6e1e2298e36539a779e6
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-01-31 21:22:24 -0500

    definición de métodos a usar en Store - aún no están implementados

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -21,6 +21,50 @@ const initStore = () => {
   console.log( store );
 }
 
+const loadStore = () => {
+  throw new Error('No implementado aún');
+} 
+
+/**
+ * Función que crea un nuevo Todo
+ * @param {String} description Descripción del Todo
+ */
+const addTodo = ( description ) => {
+  throw new Error('No implementado aún');
+}
+
+/**
+ * Función que se encarga de cambiar el estado de completado de un Todo
+ * @param {String} todoId Id del Todo
+ */
+const toggleTodo = ( todoId) => {
+  throw new Error('No implementado aún');
+}
+
+const deleteTodo = ( todoId ) => {
+  throw new Error('No implementado aún');
+}
+
+const deleteCompleted = () => {
+  throw new Error('No implementado aún');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+  throw new Error('No implementado aún');
+}
+
+const getCurrentFilter = () => {
+  throw new Error('No implementado aún');
+}
+
+
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 }
